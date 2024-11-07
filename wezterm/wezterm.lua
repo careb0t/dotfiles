be1a738385520c16eb1624c4f2257988b727b04b
careb0t <gagebond77@gmail.com>
2024-11-06 17:10:43 -0700

    added thunar extensions

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,6 @@ local config = {}
 
 config.enable_tab_bar = false
 config.color_scheme = 'Black Metal (base16)'
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.70
 
 return config
