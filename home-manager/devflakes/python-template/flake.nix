75b8d91e7e1a50263c6af297125e7272d7a49d14
careb0t <obadere@gmail.com>
2025-04-23 12:08:17 -0700

    updated Python dev flake to provide better comments and section for shared system libraries

--- home-manager/devflakes/python-template/flake.nix
+++ home-manager/devflakes/python-template/flake.nix
@@ -1,4 +1,5 @@
 {
+{
   description = "Python development environment";
 
   inputs = {
@@ -20,16 +21,25 @@
         in
         pkgs.mkShell {
           packages = with pkgs; [
+            # add Python version and Python packages here (if having issues with a package like OpenCV, try installing via pip instead)
             python3
           ];
 
+          env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            # when missing  shared system libraries like libz.so.1 or libGL.so.1, add them below
+            pkgs.stdenv.cc.cc.lib
+            pkgs.libz
+            pkgs.libGL
+            pkgs.glib
+          ];
+
           shellHook = ''
-            																		python -m venv env 
-            																		source ./env/bin/activate
-                                    						clear
-                        												echo ""
-                                    						echo "Welcome to your declarative Python development environment!"
-                                                python --version
+            python -m venv env
+            source ./env/bin/activate
+            clear
+            echo ""
+            echo "Welcome to your declarative Python development environment!"
+            python --version
           '';
         };
     };
