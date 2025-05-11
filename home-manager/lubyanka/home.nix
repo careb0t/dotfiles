296ebdf926779f61a2c1704d183a6438300beed6
careb0t <gagebond77@gmail.com>
2025-05-11 08:08:40 -0700

    removed retarded indentation on zsh initContent

--- home-manager/lubyanka/home.nix
+++ home-manager/lubyanka/home.nix
@@ -175,31 +175,31 @@
       hm-clean = "nix-collect-garbage -d";
     };
     initContent = ''
-                                    pushdots() {
-                                        cd /home/careb0t/dotfiles
-                                        git add -A
-                                        if [ "$1" != "" ]
-                                        then
-                                            git commit -m "$*"
-                                        else
-                                            git commit -m update
-                                        fi
-                                        git push
-                                    }
+      pushdots() {
+        cd /home/careb0t/dotfiles
+        git add -A
+        if [ "$1" != "" ]
+          then
+            git commit -m "$*"
+          else
+            git commit -m update
+        fi
+        git push
+      }
 
-                              			nix-dev() {
-                                      nix flake init -t devflakes#$1
-                              				if [ $? -ne 0 ]; then
-            														echo The following templates are available:
-                  											for dir in /home/careb0t/dotfiles/home-manager/devflakes/*/; do
-                          								echo $(basename $dir)
-                      									done
-      																else
-      																	direnv allow
-                                			fi
-                              			}
+      nix-dev() {
+        nix flake init -t devflakes#$1
+        if [ $? -ne 0 ]; then
+          echo The following templates are available:
+          for dir in /home/careb0t/dotfiles/home-manager/devflakes/*/; do
+            echo $(basename $dir)
+          done
+        else
+          direnv allow
+        fi
+      }
 
-                                    eval "$(direnv hook zsh)"
+      eval "$(direnv hook zsh)"
     '';
     history.size = 10000;
     history.ignoreAllDups = true;
