df47563bf75a35bc47db460e45cd3e9e1f89737b
careb0t <gagebond77@gmail.com>
2025-05-21 13:15:57 -0700

    added openmw and derivation file back to hm

--- home-manager/lubyanka/home.nix
+++ home-manager/lubyanka/home.nix
@@ -52,6 +52,7 @@
     packages =
       let
         ad-strawberry-numix-icons = pkgs.callPackage ../icons.nix { };
+        openmw = pkgs.callPackage ./openmw.nix { };
       in
       [
         ad-strawberry-numix-icons # derivation for red icon pack
