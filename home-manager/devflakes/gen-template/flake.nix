d52698b01895441067675889170b8b9dee878bf1
careb0t <gagebond77@gmail.com>
2025-06-27 20:28:53 -0700

    added new comments to general devflake

--- home-manager/devflakes/gen-template/flake.nix
+++ home-manager/devflakes/gen-template/flake.nix
@@ -22,11 +22,18 @@
           packages = with pkgs; [
             # insert runtimes, libraries, etc here
           ];
+
+          env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            # when missing  shared system libraries like libz.so.1 or libGL.so.1, add them below
+            # pkgs.stdenv.cc.cc.lib
+            # pkgs.libz
+            # pkgs.libGL
+            # pkgs.glib
+          ];
+
           shellHook = ''
-            																		echo ""
-                        												clear
-                                    						echo "Welcome to your declarative _ development environment!"
-                                                
+            # use if unable to run Node packages from terminal: PATH="$(realpath node_modules/.bin):$PATH"
+            echo "Welcome to your declarative _ development environment!"
           '';
         };
     };
