7af36dd70bb7e45513330b2e6ef041ca7347f6a6
careb0t <gagebond77@proton.me>
2025-11-28 09:55:58 -0700

    new nvim transparency lualine setup

--- .config/nvim/plugin/after/transparency.lua
+++ .config/nvim/plugin/after/transparency.lua
@@ -25,47 +25,147 @@ vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
 -- transparent lualine
 
 -- set transparent for statusline / lualine groups
-local _lualine_hls = {
-	"StatusLine",
-	"StatusLineNC",
-	-- basic lualine groups
-	"lualine_a_inactive",
-	"lualine_b_command",
-	"lualine_b_inactive",
-	"lualine_b_insert",
-	"lualine_b_normal",
-	"lualine_b_replace",
-	"lualine_b_visual",
-	"lualine_c_normal",
-	"lualine_x_normal",
-	-- c_13 and c_16 groups
-	"lualine_c_13_command","lualine_c_13_inactive","lualine_c_13_insert","lualine_c_13_normal","lualine_c_13_replace","lualine_c_13_terminal","lualine_c_13_visual",
-	"lualine_c_16_LV_Bold_command","lualine_c_16_LV_Bold_inactive","lualine_c_16_LV_Bold_insert","lualine_c_16_LV_Bold_normal","lualine_c_16_LV_Bold_replace","lualine_c_16_LV_Bold_terminal","lualine_c_16_LV_Bold_visual",
-	-- diagnostics & filetype groups
-	"lualine_c_diagnostics_error_command","lualine_c_diagnostics_error_inactive","lualine_c_diagnostics_error_insert","lualine_c_diagnostics_error_normal","lualine_c_diagnostics_error_replace","lualine_c_diagnostics_error_terminal","lualine_c_diagnostics_error_visual",
-	"lualine_c_diagnostics_warn_command","lualine_c_diagnostics_warn_inactive","lualine_c_diagnostics_warn_insert","lualine_c_diagnostics_warn_normal","lualine_c_diagnostics_warn_replace","lualine_c_diagnostics_warn_terminal","lualine_c_diagnostics_warn_visual",
-	"lualine_c_diagnostics_info_command","lualine_c_diagnostics_info_inactive","lualine_c_diagnostics_info_insert","lualine_c_diagnostics_info_normal","lualine_c_diagnostics_info_replace","lualine_c_diagnostics_info_terminal","lualine_c_diagnostics_info_visual",
-	"lualine_c_diagnostics_hint_command","lualine_c_diagnostics_hint_inactive","lualine_c_diagnostics_hint_insert","lualine_c_diagnostics_hint_normal","lualine_c_diagnostics_hint_replace","lualine_c_diagnostics_hint_terminal","lualine_c_diagnostics_hint_visual",
-	"lualine_c_filetype_MiniIconsGreen_command","lualine_c_filetype_MiniIconsGreen_inactive","lualine_c_filetype_MiniIconsGreen_insert","lualine_c_filetype_MiniIconsGreen_normal","lualine_c_filetype_MiniIconsGreen_replace","lualine_c_filetype_MiniIconsGreen_terminal","lualine_c_filetype_MiniIconsGreen_visual",
-	"lualine_c_filetype_MiniIconsYellow_command","lualine_c_filetype_MiniIconsYellow_inactive","lualine_c_filetype_MiniIconsYellow_insert","lualine_c_filetype_MiniIconsYellow_normal","lualine_c_filetype_MiniIconsYellow_replace","lualine_c_filetype_MiniIconsYellow_terminal","lualine_c_filetype_MiniIconsYellow_visual",
-	-- transitional and x/diff groups
-	"lualine_transitional_lualine_a_command_to_lualine_b_command","lualine_transitional_lualine_a_normal_to_lualine_b_normal","lualine_transitional_lualine_a_normal_to_lualine_c_normal",
-	"lualine_x_3_command","lualine_x_3_inactive","lualine_x_3_insert","lualine_x_3_normal","lualine_x_3_replace","lualine_x_3_terminal","lualine_x_3_visual",
-	"lualine_x_4_command","lualine_x_4_inactive","lualine_x_4_insert","lualine_x_4_normal","lualine_x_4_replace","lualine_x_4_terminal","lualine_x_4_visual",
-	"lualine_x_5_command","lualine_x_5_inactive","lualine_x_5_insert","lualine_x_5_normal","lualine_x_5_replace","lualine_x_5_terminal","lualine_x_5_visual",
-	"lualine_x_6_command","lualine_x_6_inactive","lualine_x_6_insert","lualine_x_6_normal","lualine_x_6_replace","lualine_x_6_terminal","lualine_x_6_visual",
-	"lualine_x_diff_added_command","lualine_x_diff_added_inactive","lualine_x_diff_added_insert","lualine_x_diff_added_normal","lualine_x_diff_added_replace","lualine_x_diff_added_terminal","lualine_x_diff_added_visual",
-	"lualine_x_diff_modified_command","lualine_x_diff_modified_inactive","lualine_x_diff_modified_insert","lualine_x_diff_modified_normal","lualine_x_diff_modified_replace","lualine_x_diff_modified_terminal","lualine_x_diff_modified_visual",
-	"lualine_x_diff_removed_command","lualine_x_diff_removed_inactive","lualine_x_diff_removed_insert","lualine_x_diff_removed_normal","lualine_x_diff_removed_replace","lualine_x_diff_removed_terminal","lualine_x_diff_removed_visual",
-	-- misc lualine groups
-	"lualine_c_normal","lualine_transparent","lualine_a_normal","lualine_a_insert","lualine_a_visual","lualine_a_command","lualine_a_replace",
-	"lualine_c_2_normal","lualine_c_2_insert","lualine_c_2_visual","lualine_c_2_replace","lualine_c_2_command","lualine_c_2_terminal","lualine_c_2_inactive",
-	"lualine_c_13_normal","lualine_c_13_insert","lualine_c_13_visual","lualine_c_13_replace","lualine_c_13_command","lualine_c_13_terminal","lualine_c_13_inactive",
-}
-
-for _, _name in ipairs(_lualine_hls) do
-	pcall(vim.api.nvim_set_hl, 0, _name, { bg = "none" })
-end
+-- explicit set_hl calls for lualine/statusline groups
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_a_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_b_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_b_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_b_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_b_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_b_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_b_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_16_LV_Bold_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_16_LV_Bold_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_16_LV_Bold_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_16_LV_Bold_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_16_LV_Bold_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_16_LV_Bold_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_16_LV_Bold_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_error_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_error_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_error_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_error_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_error_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_error_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_error_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_warn_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_warn_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_warn_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_warn_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_warn_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_warn_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_warn_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_info_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_info_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_info_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_info_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_info_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_info_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_info_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_hint_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_hint_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_hint_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_hint_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_hint_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_hint_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_diagnostics_hint_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsGreen_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsGreen_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsGreen_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsGreen_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsGreen_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsGreen_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsGreen_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsYellow_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsYellow_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsYellow_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsYellow_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsYellow_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsYellow_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_filetype_MiniIconsYellow_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_transitional_lualine_a_command_to_lualine_b_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_transitional_lualine_a_normal_to_lualine_b_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_transitional_lualine_a_normal_to_lualine_c_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_3_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_3_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_3_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_3_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_3_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_3_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_3_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_4_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_4_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_4_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_4_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_4_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_4_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_4_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_5_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_5_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_5_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_5_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_5_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_5_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_5_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_6_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_6_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_6_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_6_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_6_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_6_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_6_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_added_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_added_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_added_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_added_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_added_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_added_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_added_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_modified_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_modified_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_modified_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_modified_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_modified_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_modified_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_modified_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_removed_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_removed_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_removed_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_removed_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_removed_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_removed_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_x_diff_removed_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_transparent", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_a_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_a_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_a_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_a_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_a_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_2_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_2_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_2_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_2_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_2_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_2_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_2_inactive", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_insert", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_visual", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_replace", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_command", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_terminal", { bg = "none" })
+vim.api.nvim_set_hl(0, "lualine_c_13_inactive", { bg = "none" })
 
 -- transparent bufferline
 vim.api.nvim_set_hl(0, "TabLineFill", { bg = "none" })
