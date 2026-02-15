d4d9300530fa175b6b5a7abb0eb490274819c1be
careb0t <gagebond77@proton.me>
2026-02-15 11:25:15 -0700

    fixed keybinds for tmux-nvim-navigator

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Override LazyVim's default window navigation keymaps with tmux-aware navigation
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,11 +1,6 @@
 return {
   "alexghergh/nvim-tmux-navigation",
   config = function()
-    local nvim_tmux_nav = require("nvim-tmux-navigation")
-    nvim_tmux_nav.setup({})
-    vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-    vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-    vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-    vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+    require("nvim-tmux-navigation").setup({})
   end,
 }
